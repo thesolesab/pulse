ef118fb8e73ddcb2f2355b2888101c4ecb1ebb95
IZI <thesolesab@gmail.com>
2020-11-10 16:42:25 +0300

    dz2

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 1000,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         autoplay: true,
         autoplaySpeed: 5000,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icon/catalog/left.png"></button>',
