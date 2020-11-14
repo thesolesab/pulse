cc688d1baf14a1be86c85ea755f58f1c66fae006
IZI <thesolesab@gmail.com>
2020-11-14 13:41:56 +0300

    debug

--- src/js/script.js
+++ src/js/script.js
@@ -109,7 +109,7 @@ $(document).ready(function() {
         }
     });
 
-    $("a[href^='#']").click(function() {
+    $("a[href^=#up]").click(function() {
         var _href = $(this).attr("href");
         $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
         return false;
